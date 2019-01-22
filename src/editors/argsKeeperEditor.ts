c56c6cf531b4e755ffc9cd2b854c69acdefdaac4
Jase Kirby <jasekirby7@gmail.com>
2019-01-21 20:16:08 -0500

    argsKeeper data editor class and unit test for add and remove rules

---
+++ src/editors/argsKeeperEditor.ts
@@ -0,0 +1,46 @@
+import { IArgsKeeper } from "../models/argsKeeper";
+import { IArgsKeeperGroup } from "../models/argsKeeperGroup";
+
+export class ArgsKeeperEditor {
+    private argsKeeper: IArgsKeeper;
+    constructor(argsKeeper: IArgsKeeper) {
+        this.argsKeeper = argsKeeper;
+    }
+
+    private doesNameKeyHaveWhitespace(nameKey: string): boolean {
+        if (nameKey.indexOf(" ") >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public addGroup(newGroup: IArgsKeeperGroup): void {
+        const groupsWithThisNameCount: number = this.argsKeeper.groups.filter(x => x.name === newGroup.name).length;
+        if(groupsWithThisNameCount > 0) {
+            throw new ArgskGroupNameAlreadyExistsError(newGroup.name);
+        }
+        if(this.doesNameKeyHaveWhitespace(newGroup.name)) {
+            throw new ArgskWhiteSpaceInNameError(newGroup.name);
+        }
+        this.argsKeeper.groups.push(newGroup);
+    }
+
+    public removeGroupByName(groupName: string): void {
+        this.argsKeeper.groups = this.argsKeeper.groups.filter(x => x.name !== groupName);
+    }
+}
+
+export class ArgskGroupNameAlreadyExistsError extends Error {
+    constructor(groupName: string) {
+        super(`Group with name ${groupName} already exists in groups. Group names must be unique.`);
+        this.name = "ArgskGroupNameAlreadyExistsError";
+    }
+}
+
+export class ArgskWhiteSpaceInNameError extends Error {
+    constructor(name: string) {
+        super(`Whitespace found in name key '${name}'. No whitespace allowed in name keys.`);
+        this.name = "ArgskWhiteSpaceInNameError";
+    }
+}
---
+++ test/main/editors/argsKeeperEditor.spec.ts
@@ -0,0 +1,53 @@
+import { IArgsKeeper, ArgsKeeper } from "../../../src/models/argsKeeper";
+import { IArgsKeeperGroup } from "../../../src/models/argsKeeperGroup";
+import { ArgsKeeperEditor, ArgskGroupNameAlreadyExistsError, ArgskWhiteSpaceInNameError } from "../../../src/editors/argsKeeperEditor";
+import * as assert from "assert";
+
+describe("ArgsKeeperEditor", () => {
+
+    it("can add group", () => {
+        const argsKeeper: IArgsKeeper = new ArgsKeeper();
+        const linuxGroup: IArgsKeeperGroup = {name: "linux"};
+        const editor: ArgsKeeperEditor = new ArgsKeeperEditor(argsKeeper);
+        editor.addGroup(linuxGroup);
+
+        assert.equal(argsKeeper.groups.length, 1);
+        assert.equal(argsKeeper.groups[0].name, "linux");
+    });
+
+    it("adding a group where a group with the same name exists throws ArgskGroupNameAlreadyExistsError", () => {
+        const argsKeeper: IArgsKeeper = new ArgsKeeper();
+        const linuxGroup: IArgsKeeperGroup = {name: "linux"};
+        const editor: ArgsKeeperEditor = new ArgsKeeperEditor(argsKeeper);
+        editor.addGroup(linuxGroup);
+
+        const addingGroupWithSameName: Function = () => editor.addGroup(linuxGroup);
+
+        assert.throws(addingGroupWithSameName, ArgskGroupNameAlreadyExistsError);
+        assert.equal(argsKeeper.groups.length, 1);
+    });
+
+    it("adding a group with whitespace in the name throws ArgskWhiteSpaceInNameError", () => {
+        const argsKeeper: IArgsKeeper = new ArgsKeeper();
+        const windowsSpaceGroup: IArgsKeeperGroup = {name: "Windows Group "};
+        const editor: ArgsKeeperEditor = new ArgsKeeperEditor(argsKeeper);
+
+        const addGroupWithWhiteSpaceName: Function = () => editor.addGroup(windowsSpaceGroup);
+
+        assert.throws(addGroupWithWhiteSpaceName, ArgskWhiteSpaceInNameError);
+        assert.equal(argsKeeper.groups.length, 0);
+    });
+
+    it("can remove group by name", () => {
+        const argsKeeper: IArgsKeeper = new ArgsKeeper();
+        const linuxGroup: IArgsKeeperGroup = {name: "linux"};
+        const windowsGroup: IArgsKeeperGroup = {name: "windows"};
+        const editor: ArgsKeeperEditor = new ArgsKeeperEditor(argsKeeper);
+        editor.addGroup(linuxGroup);
+        editor.addGroup(windowsGroup);
+        editor.removeGroupByName(linuxGroup.name);
+
+        assert.equal(argsKeeper.groups.length, 1);
+        assert.equal(argsKeeper.groups[0].name, windowsGroup.name);
+    });
+});
